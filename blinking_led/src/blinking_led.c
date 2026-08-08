c6efabb67e20e960dca8c47b4574c4e207d2d26a
Henry Centeno <centenohenry123@gmail.com>
2026-08-07 20:28:17 -0400

    commiting

--- blinking_led/src/blinking_led.c
+++ blinking_led/src/blinking_led.c
@@ -6,28 +6,23 @@
 #include "stm32g474xx.h"
 #include <inttypes.h>
 
-#define GPIOA_BSRR (*((volatile uint32_t *)0x48000018ul)) //bsrr register for GPIOA
-#define RCC_AHB2ENR (*((volatile uint32_t *)0x4002104Cul)) //gpioa's clock
-#define GPIOA_MODER (*((volatile uint32_t *)0x48000000ul)) //moder register for GPIOA
-
 void delay(uint32_t time){
     while(time--){}
 }
 
 int main(void){
     //setup
-    RCC_AHB2ENR |= (1 << 0); //enable the clock bit while leaving every other bit untouched
-    GPIOA_MODER &= ~(0b11 << 10); //mode bits 11 and 10 of PA5, clear both of these bits
-    GPIOA_MODER |= (0b01 << 10);  //set the mode bits to 01
-
-    
-    uint8_t pinBit = 5; //PA5
-    uint8_t pa_offset = 16;
+    GPIOA->MODER &= ~GPIO_MODER_MODE5; //mode bits 11 and 10 of PA5, clear both of these bits
+    GPIOA->MODER |= GPIO_MODER_MODE5_0; //set the mode bits to 01
+    GPIOA->MODER &= ~GPIO_MODER_MODE8; //mode bits 11 and 10 of PA5, clear both of these bits
+    GPIOA->MODER |= GPIO_MODER_MODE8_0; //set the mode bits to 01
 
     while(1){
-        GPIOA_BSRR = (1 << pinBit); //set the pin high
+        // GPIOA_BSRR = (1 << pinBit) | (1 << ld2); //set the pin high
+        GPIOA->BSRR = GPIO_BSRR_BS5 | GPIO_BSRR_BS8;
         delay(1000000);
-        GPIOA_BSRR = (1 << (pinBit + pa_offset)); //set the pin low
+        // GPIOA_BSRR = (1 << (pinBit + pa_offset)) | (1 << (ld2 + pa_offset)); //set the pin low
+        GPIOA->BSRR = GPIO_BSRR_BR5 | GPIO_BSRR_BR8;
         delay(500000);
     }
 }
